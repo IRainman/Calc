0809811f37fdd7b2b6aede0a05a10e60626abda7
HedgehogInTheCPP <a.rainman@gmail.com>
2009-10-24 13:21:14 +0000

    * добавил суслико устойчивость
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@41 bea4479c-5f86-c742-a837-c53f34f90b20

--- Calc/Calculator.cpp
+++ Calc/Calculator.cpp
@@ -560,8 +560,16 @@ bool CalculateLineExpression(string p_input_str, string& p_output_str)
 	if(!c_operands.empty())
 	{
 		char l_char_buf[100];
-		sprintf_s(l_char_buf, "%lf", c_operands.top());
-		p_output_str = l_char_buf;
+		try
+		{
+			sprintf_s(l_char_buf, "%lf", c_operands.top());
+			p_output_str = l_char_buf;
+		}
+		catch(...)
+		{
+			is_ok = false;
+			m_ErrorString += L"Суслик, ты балбес!!! число слишком большое\r\n";
+		}
 	}
 	return is_ok;
 }
