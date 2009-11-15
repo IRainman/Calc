29886c8cd9477b883e4508e5e78a3a115ab4af5b
HedgehogInTheCPP <a.rainman@gmail.com>
2009-11-15 15:04:21 +0000

    * исправлено возможное падение в калькуляторе
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@84 bea4479c-5f86-c742-a837-c53f34f90b20

--- Calc/RPN.cpp
+++ Calc/RPN.cpp
@@ -262,7 +262,7 @@ void CalculateLineExpression(string p_input_str, string& p_output_str)
 	}
 	if(!c_operands.empty())
 	{
-		char l_char_buf[320];
+		char l_char_buf[400];
 		sprintf_s(l_char_buf, "%.14lf", c_operands.top()); // TODO: Add variable precision
 		p_output_str = l_char_buf;
 	}
