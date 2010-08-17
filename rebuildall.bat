a730304f510633232a6f3d3d291170471eb856df
HedgehogInTheCPP <a.rainman@gmail.com>
2010-08-17 19:57:33 +0000

    * Исправил опечатку в батнике пересборки проектов
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@124 bea4479c-5f86-c742-a837-c53f34f90b20

--- rebuildall.bat
+++ rebuildall.bat
@@ -1,5 +1,5 @@
 call "%VS100COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
 "%VS100COMNTOOLS%..\ide\devenv" Calc.sln /Rebuild "Release|Win32"
 
-call "%VS100COMNTOOLS%\..\..\VC\bin\amd64\vcvarsamd64.bat"
+call "%VS100COMNTOOLS%\..\..\VC\bin\amd64\vcvars64.bat"
 "%VS100COMNTOOLS%..\ide\devenv" Calc.sln /Rebuild "Release|x64"
