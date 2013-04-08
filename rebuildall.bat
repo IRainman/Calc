512eebb878ede26e14f2b478b171716f35a84e12
HedgehogInTheCPP <a.rainman@gmail.com>
2013-04-08 18:49:53 +0000

    * Версия 1.4.0.138. * Сборка в Visual Studio 2012 update 2. # Поправил батник сборки (забыл обновить пути к переменным среды).
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@138 bea4479c-5f86-c742-a837-c53f34f90b20

--- rebuildall.bat
+++ rebuildall.bat
@@ -1,5 +1,5 @@
-call "%VS100COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
+call "%VS110COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
 "%VS110COMNTOOLS%..\ide\devenv" Calc.sln /Rebuild "Release|Win32"
 
-call "%VS100COMNTOOLS%\..\..\VC\bin\amd64\vcvars64.bat"
+call "%VS110COMNTOOLS%\..\..\VC\bin\amd64\vcvars64.bat"
 "%VS110COMNTOOLS%..\ide\devenv" Calc.sln /Rebuild "Release|x64"
