0575cddc72f45c54f630d82a53161924b35cdd81
HedgehogInTheCPP <a.rainman@gmail.com>
2023-06-02 20:16:58 +0000

    # Disable exception for whole project. # Small memory footprint cleanup.
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@194 bea4479c-5f86-c742-a837-c53f34f90b20

--- Calc/MyTypes.h
+++ Calc/MyTypes.h
@@ -21,13 +21,13 @@
 // TODO 128 bit is hardware supported on x86 and ARM. Need fix from compiler.
 typedef long double calc_variable;
 //---------------------------------------------------------------------------
-constexpr auto CALC_BUFFER_SIZE = 64;
+constexpr auto CALC_BUFFER_SIZE = 32;
 //---------------------------------------------------------------------------
 template<typename T>
 inline constexpr T& print_value(T& p_str, const calc_variable p_val)
 {
 	p_str.resize(CALC_BUFFER_SIZE);
-	p_str.resize(to_chars(p_str.data(), p_str.data() + p_str.size(), p_val).ptr - p_str.data());
+	p_str.resize(std::to_chars(p_str.data(), p_str.data() + p_str.size(), p_val).ptr - p_str.data());
 	return p_str;
 }
 //---------------------------------------------------------------------------
