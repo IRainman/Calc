2ffc684989afe1f665d199a65390a8b379d2f570
HedgehogInTheCPP <a.rainman@gmail.com>
2023-06-03 22:10:45 +0000

    * Unification of the hypot2 and hypot3 function. Now it's the function named hypot for 2 and 3 parameters. * Now ANSI synonyms for functions log, log10, log2 can be use: ln for log; lg for log10; lb for log2. * Added the function riemann_zeta. * Added the constant egamma. * Improvements for error handling when calling a function with variadic parameters. Currently this is min and max functions.
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@199 bea4479c-5f86-c742-a837-c53f34f90b20

--- Calc/parser.cpp
+++ Calc/parser.cpp
@@ -19,43 +19,73 @@ using Value = long double;
 
 struct Fn
 {
+	struct P
+	{
+		constexpr static auto unlim = std::numeric_limits<size_t>::max();
+		/**
+		 * Test is Fn is a constant and no needs arguments.
+		 */
+		constexpr bool is_constant() const
+		{
+			return min == 0 && max == 0;
+		}
+		/**
+		 * Test is Fn is a function and needs arguments.
+		 */
+		constexpr bool is_function() const
+		{
+			return !is_constant();
+		}
+		/**
+		 * Test interval of paramethers count.
+		 */
+		constexpr bool params_count_is_valid(const size_t count) const
+		{
+			return min <= count || count <= max;
+		}
+		const size_t min; //-V122
+		const size_t max; //-V122
+	} check;
 	Value(*fn)(std::span<const Value>); //-V122
-	const size_t n; //-V122
 };
 
-template <const size_t I>
+template <const size_t>
 using WrappedFnImplArg = Value;
+
 template <const size_t... Is>
-auto WrappedFnImpl(std::index_sequence<Is...>) noexcept -> Value(*)(WrappedFnImplArg<Is>...);
+consteval auto WrappedFnImpl(std::index_sequence<Is...>) noexcept -> Value(*)(WrappedFnImplArg<Is>...);
+
 template <const size_t N>
 using WrappedFn = decltype(WrappedFnImpl(std::make_index_sequence<N>()));
+
 template <typename Fn, const size_t... Is>
-Value call_fn(Fn fn, std::span<const Value>& params, std::index_sequence<Is...>) noexcept
+constexpr auto call_fn(Fn fn, std::span<const Value> params, std::index_sequence<Is...>) noexcept
 {
 	return fn(params[Is]...);
 }
+
 template<const size_t N, WrappedFn<N> wrappedFn>
-Value function_pointer_impl(std::span<const Value> params) noexcept
+constexpr auto function_pointer_impl(std::span<const Value> params) noexcept
 {
 	return call_fn(wrappedFn, params, std::make_index_sequence<N>());
 }
 
 template<const size_t N, WrappedFn<N> wrappedFn>
-constexpr Fn function_pointer() noexcept
+consteval Fn function_pointer() noexcept
 {
-	return { function_pointer_impl<N, wrappedFn>, N };
+	return { { N, 0 }, function_pointer_impl<N, wrappedFn> };
 }
 
 template <const Value value>
-Value constant_impl(std::span<const Value>) noexcept
+constexpr auto constant_impl(std::span<const Value>) noexcept
 {
 	return value;
 }
 
 template <const Value value>
-constexpr Fn constant() noexcept
+consteval Fn constant() noexcept
 {
-	return { constant_impl<value>, 0 };
+	return { { 0, 0 }, constant_impl<value> };
 }
 
 constexpr Value min(std::span<const Value> params) noexcept
@@ -71,84 +101,98 @@ constexpr Value max(std::span<const Value> params) noexcept
 constexpr Value rad(const Value x) noexcept { return x * std::numbers::pi_v<Value> / 180; }
 constexpr Value deg(const Value x) noexcept { return x * 180 / std::numbers::pi_v<Value>; }
 
-constexpr auto IDENTIFIERS_unlimited_params = std::numeric_limits<size_t>::max();
+constexpr Value hypot(std::span<const Value> params) noexcept
+{
+	switch (params.size())
+	{
+		case 2:
+			return std::hypot(params[0], params[1]);
+		case 3:
+			return std::hypot(params[0], params[1], params[2]);
+		default:
+			return NAN;
+	};
+}
 
-const std::unordered_map<std::string_view, Fn> IDENTIFIERS =
+constexpr Value log(Value x) noexcept
+{
+	if (x-- < 1)
+	{
+		return std::log1p(--x);
+	}
+	else
+	{
+		return std::log(x);
+	}
+}
+
+static const /*TODO constinit*/ std::unordered_map<std::string_view, const Fn> IDENTIFIERS =
 {
 	//---------------------------------------------------------------------------
 	// TODO https://en.cppreference.com/w/cpp/numeric/constants
-	{"pi", constant<std::numbers::pi_v<Value>>()},// Pi, Archimedes' constant or Ludolph's number
-	{"e", constant<std::numbers::e_v<Value>>()},// Euler's number
-	{"phi", constant<std::numbers::phi_v<Value>>()},// Golden ratio
+	{"pi", constant<std::numbers::pi>()},
+	{"e", constant<std::numbers::e>()},
+	{"phi", constant<std::numbers::phi>()},
+	{"egamma", constant<std::numbers::egamma>()},
 	//---------------------------------------------------------------------------
 	// TODO additional constant
 	{"c", constant<299792458.0L>()},// Speed of light in vacuum (m·s-1)
-	{"G", constant<6.674286767676767676767676767676767676e-11L> ()}, // Newtonian constant of gravitation (m3·kg−1·s−2), this constant valid on Earths only ;)
+	{"G", constant<6.674286767676767676767676767676767676e-11L>()},// Newtonian constant of gravitation (m3·kg−1·s−2), this constant valid on Earths only ;)
 	{"J", constant<3.058198247456354132564564787888767L>()},// Constants of Gauss fild
 	{"atm", constant<101325.0L>()},// Standard atmosphere (Pa), this constant valid on Earths only ;)
 	{"L", constant<6.022141510101010101010101010101010101e23L>()},// Avogadro's number (mol−1)
 	{"R", constant<8.314472151515151515151515151515151515L>()},// Gas constant (J·K−1·mol−1)
-	{"h", constant<6.626068963333333333333333333333333333e-34L> ()}, // Planck constant (J·s)
+	{"h", constant<6.626068963333333333333333333333333333e-34L>()},// Planck constant (J·s)
 	//---------------------------------------------------------------------------
 	// TODO https://en.cppreference.com/w/cpp/numeric/math
-	// TODO https://en.cppreference.com/w/cpp/numeric/special_math
-	{"sin", function_pointer<1, std::sin>()},// sinus
-	{"cos", function_pointer<1, std::cos>()},// cosinus
-	{"tan", function_pointer<1, std::tan>()},// tangens
-	{"arccos", function_pointer<1, std::acos>()},// arccosinus
-	{"arcsin", function_pointer<1, std::asin>()},// arcsinus
-	{"arctan", function_pointer<1, std::atan>()},// arctangens
+	{"sin", function_pointer<1, std::sin>()},
+	{"cos", function_pointer<1, std::cos>()},
+	{"tan", function_pointer<1, std::tan>()},
+	{"arccos", function_pointer<1, std::acos>()},
+	{"arcsin", function_pointer<1, std::asin>()},
+	{"arctan", function_pointer<1, std::atan>()},
 	
-	// TODO merge this into one function
-	{"hypot2", function_pointer<2, std::hypot>()},
-	{"hypot3", function_pointer<3, std::hypot>()},// https://en.cppreference.com/w/cpp/numeric/math/hypot
+	{"hypot", {{2, 3}, hypot}},
 	
-	{"exp", function_pointer<1, std::exp>()},// exponent function, on x=1 return value is e
+	{"exp", function_pointer<1, std::exp>()},
 	
-	{"sh", function_pointer<1, std::sinh>()},// https://en.cppreference.com/w/cpp/numeric/math/sinh
-	{"ch", function_pointer<1, std::cosh>()},// https://en.cppreference.com/w/cpp/numeric/math/cosh
-	{"tanh", function_pointer<1, std::tanh>()},// https://en.cppreference.com/w/cpp/numeric/math/tanh
-	{"asinh", function_pointer<1, std::asinh>()},// https://en.cppreference.com/w/cpp/numeric/math/asinh
-	{"acosh", function_pointer<1, std::acosh>()},// https://en.cppreference.com/w/cpp/numeric/math/acosh
-	{"atanh", function_pointer<1, std::atanh>()},// https://en.cppreference.com/w/cpp/numeric/math/atanh
+	{"sh", function_pointer<1, std::sinh>()},
+	{"ch", function_pointer<1, std::cosh>()},
+	{"tanh", function_pointer<1, std::tanh>()},
+	{"asinh", function_pointer<1, std::asinh>()},
+	{"acosh", function_pointer<1, std::acosh>()},
+	{"atanh", function_pointer<1, std::atanh>()},
 	
-	// TODO merge this into one function
-	{"log", function_pointer<1, std::log>()},// natural logarithm
-	{"log10", function_pointer<1, std::log10>()},// base-10 logarithm
-	{"log2", function_pointer<1, std::log2>()},// https://en.cppreference.com/w/cpp/numeric/math/log2
-	{"log1p", function_pointer<1, std::log1p>()},// https://en.cppreference.com/w/cpp/numeric/math/log1p
+	{"ln", function_pointer<1, log>()},
+	{"log", function_pointer<1, log>()},
+
+	{"lg", function_pointer<1, std::log10>()},
+	{"log10", function_pointer<1, std::log10>()},
+
+	{"lb", function_pointer<1, std::log2>()},
+	{"log2", function_pointer<1, std::log2>()},
 	
-	// TODO merge this into one function
-	{"sqrt", function_pointer<1, std::sqrt>()},// square root https://en.cppreference.com/w/cpp/numeric/math/sqrt
-	{"cbrt", function_pointer<1, std::cbrt>()},// https://en.cppreference.com/w/cpp/numeric/math/cbrt
-	{"pow", function_pointer<2, std::pow>()},// power
+	{"sqrt", function_pointer<1, std::sqrt>()},
+	{"cbrt", function_pointer<1, std::cbrt>()},
+	{"pow", function_pointer<2, std::pow>()},
 	
-	{"rad", function_pointer<1, rad>()},// One radian is equivalent to 180/PI degrees.
+	{"rad", function_pointer<1, rad>()},
 	{"deg", function_pointer<1, deg>()},
 	
-	{"min", {min, IDENTIFIERS_unlimited_params}},
-	{"max", {max, IDENTIFIERS_unlimited_params}},
+	{"min", {{1, Fn::P::unlim}, min}},
+	{"max", {{1, Fn::P::unlim}, max}},
 	
-	{"abs", function_pointer<1, abs>()},// https://en.cppreference.com/w/cpp/numeric/math/fabs
+	{"abs", function_pointer<1, abs>()},
 	
-	{"tgamma", function_pointer<1, tgamma>()}, // https://en.cppreference.com/w/cpp/numeric/math/tgamma
-	{"lgamma", function_pointer<1, lgamma>()}, // https://en.cppreference.com/w/cpp/numeric/math/lgamma
+	{"tgamma", function_pointer<1, tgamma>()},
+	{"lgamma", function_pointer<1, lgamma>()},
 	
-	{"trunc", function_pointer<1, trunc>()}, // https://en.cppreference.com/w/cpp/numeric/math/trunc
-	{"round", function_pointer<1, round>()}, // https://en.cppreference.com/w/cpp/numeric/math/round
+	{"trunc", function_pointer<1, trunc>()},
+	{"round", function_pointer<1, round>()},
 	
-	//ceil,
-	//fabs,
-	//floor,
-	//ldexp,
-	//modf,
-	//atan2,
-	//modf,
-	//fmod,
-	//frexp,
-	//gcd,// https://en.cppreference.com/w/cpp/numeric/gcd
-	//lcm,// https://en.cppreference.com/w/cpp/numeric/lcm
-	//
+	//---------------------------------------------------------------------------
+	// TODO https://en.cppreference.com/w/cpp/numeric/special_math
+	{"riemann_zeta", function_pointer<1, std::riemann_zeta>()},// https://en.cppreference.com/w/cpp/numeric/special_functions/riemann_zeta
 	//---------------------------------------------------------------------------
 };
 }
@@ -293,8 +337,10 @@ long double Parser::parse_expr_0()
 			return parse_function_or_constant();
 		}
 		default:
+		{
 			IssueManager::get_instance().report_error(_lex.get_position(), std::format("unexpected {}", _current.text));
 			return NAN;
+		}
 	}
 }
 
@@ -308,42 +354,44 @@ long double Parser::parse_function_or_constant()
 		advance();
 		if (const auto i = IDENTIFIERS.find(name); i != IDENTIFIERS.end())
 		{
-			const auto [function, N] = i->second;
-			if (!N)
+			const auto [check, function] = i->second;
+			if (check.is_function())
 			{
-				IssueManager::get_instance().report_error(_lex.get_position(), std::format("identifier {} is a constant, not a function", name));
-				return NAN;
-			}
-			
-			std::vector<long double> params;
-			
-			while (true)
-			{
-				params.push_back(parse_expr_4());
-				
-				if (_current.type == Token::Type::RPAREN)
+				std::vector<long double> params;
+
+				while (true)
 				{
-					advance();
-					break;
+					params.push_back(parse_expr_4());
+
+					if (_current.type == Token::Type::RPAREN)
+					{
+						advance();
+						break;
+					}
+					else if (_current.type == Token::Type::COMA)
+					{
+						advance();
+						continue;
+					}
+					else
+					{
+						IssueManager::get_instance().report_error(_lex.get_position(), std::format("expected closing paren or coma, got {}", _current.text));
+						return NAN;
+					}
 				}
-				else if (_current.type == Token::Type::COMA)
+				if (check.params_count_is_valid(params.size()))
 				{
-					advance();
-					continue;
+					return function(params);
 				}
 				else
 				{
-					IssueManager::get_instance().report_error(_lex.get_position(), std::format("expected closing paren or coma, got {}", _current.text));
+					IssueManager::get_instance().report_error(pos_of_ident_start, std::format("for function {} expected minimum {} and maximum {} paramethers, got {}", name, check.min, check.max, params.size()));
 					return NAN;
 				}
 			}
-			if (N == IDENTIFIERS_unlimited_params || N == params.size())
-			{
-				return function(params);
-			}
 			else
 			{
-				IssueManager::get_instance().report_error(pos_of_ident_start, std::format("for function {} expected {} paramethers, got {}", name, N, params.size()));
+				IssueManager::get_instance().report_error(pos_of_ident_start, std::format("identifier {} is not a function", name));
 				return NAN;
 			}
 		}
@@ -357,14 +405,14 @@ long double Parser::parse_function_or_constant()
 	{
 		if (const auto i = IDENTIFIERS.find(name); i != IDENTIFIERS.end())
 		{
-			const auto [constant, N] = i->second;
-			if (!N)
+			const auto [check, constant] = i->second;
+			if (check.is_constant())
 			{
-				return constant( {});
+				return constant({});
 			}
 			else
 			{
-				IssueManager::get_instance().report_error(pos_of_ident_start, std::format("function needs paramethers {}", name));
+				IssueManager::get_instance().report_error(pos_of_ident_start, std::format("function {} needs paranthesis for call", name));
 				return NAN;
 			}
 		}
