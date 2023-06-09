fb59a3969268f5909759df793aebb26595c026d8
HedgehogInTheCPP <a.rainman@gmail.com>
2023-06-09 16:40:53 +0000

    # Tests are updated.
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@206 bea4479c-5f86-c742-a837-c53f34f90b20

--- Calc/tests.h
+++ Calc/tests.h
@@ -16,6 +16,7 @@ auto calc_tests()
 {
 	const std::vector<std::pair<std::string, std::string>> tests =   //-V826
 	{
+		// syntax errors
 		{"2 + )", "nan"},
 		{"2 + (", "nan"},
 		{"e(", "nan"},
@@ -23,26 +24,38 @@ auto calc_tests()
 		{"sin(0 ", "nan"},
 		{"sin(", "nan"},
 		
+		// value parsing
 		{"1.4e-3", "0.0014"},
 		
+		// operation priority
 		{"2 + 2", "4"},
 		{"2 * 2", "4"},
 		{"2 + 2 * 2", "6"},
 		{"(2 + 2) * 2", "8"},
+
+		// precision
+		{"10000 / 540 * 3", "55.555555555556"},
+		{"1 / 3 * 3", "1"},
 		
+		// special value support
 		{"1 / 0", "inf"},
 		
+		// trigonometric functions
 		{"sin( rad(0) )", "0"},
+		{"87 * tan(pi) - 7", "-7"},
 		
 		{"log(sh(42) + ch(42))", "42"},
 		
+		// power and root functions
 		{"pow( sin( pi / 2 ) / .001 + 24, 2 )", "1048576"},
 		{"0^0", "1"},
+		{"-4 ^ 2", "16"},
 		{"- 4 ^ 2", "16"},
 		{"0 - 4 ^ 2", "-16"},
 		{"3 ^ 3 ^ 3", "7625597484987"},
 		{"(3 ^ 3) ^ 3", "19683"},
 		
+		// modulo operator
 		{"-5 % 3", "-2"},
 		{"5 % -3", "2"},
 		{"-5 % -3", "-2"},
@@ -53,6 +66,7 @@ auto calc_tests()
 		{"6 / 3 % 2", "0"},
 		{"6 % 4 / 2", "1"},
 		
+		// min max functions
 		{"min(1)", "1"},
 		{"min(1, 2, 3)", "1"},
 		{"max(1, 2, 3)", "3"},
