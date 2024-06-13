e8381ffbd6ac40f95ea45147fc3fec9b935b2b3e
HedgehogInTheCPP <a.rainman@gmail.com>
2024-06-13 21:30:28 +0000

    # Cleanup very old version (0.x and 1.0) from code.
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@228 bea4479c-5f86-c742-a837-c53f34f90b20

--- Calc/Flags.h
+++ Calc/Flags.h
@@ -4,20 +4,6 @@
  */
 
 //---------------------------------------------------------------------------
-#define CALC_VER2
-//---------------------------------------------------------------------------
-#ifndef CALC_VER2
-	// Deprecated because is very slow and get high value of cyclomatic complexity https://en.wikipedia.org/wiki/Cyclomatic_complexity
-	//#define ENABLE_INPUT_VALIDATION // Deprecated
-
-	#ifdef ENABLE_LOG
-	//#define EXTENDENT_REPORT_OF_POSITION_IN_LOG
-	#ifdef _DEBUG
-	//#define ENABLE_LOG_DEBUG
-	#endif // _DEBUG
-	#endif // ENABLE_LOG
-#endif
-//---------------------------------------------------------------------------
 #ifdef _DEBUG
 #define CALC_TESTS_ENABLED
 #endif
