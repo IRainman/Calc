ba30500ff2ee3263df334aab043bfce3fa924ed1
HedgehogInTheCPP <a.rainman@gmail.com>
2024-12-16 11:05:47 +0000

    * Version 2.1.6.250. * Enable parallel buildings in building script. * Enable assembly generation for release builds by default for post build analysis. * /LARGEADDRESSAWARE:NO * Build in Visual Studio 2022 (17.12.3).
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@250 bea4479c-5f86-c742-a837-c53f34f90b20

--- rebuildall.bat
+++ rebuildall.bat
@@ -1,5 +1,5 @@
 call "c:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
 
-devenv Calc.sln /Rebuild "Release|Win32"
+start devenv Calc.sln /Rebuild "Release|Win32"
 
-devenv Calc.sln /Rebuild "Release|x64"
+start devenv Calc.sln /Rebuild "Release|x64"
