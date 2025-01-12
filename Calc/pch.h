d623259a5a261f95c34555342feb99e236cddc44
HedgehogInTheCPP <a.rainman@gmail.com>
2025-01-12 11:08:59 +0000

    # includes sorted and cleanup.
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@294 bea4479c-5f86-c742-a837-c53f34f90b20

--- Calc/pch.h
+++ Calc/pch.h
@@ -3,21 +3,23 @@
  * Copyright 2009-2024 Solomina Elle Leonovna, a.rainman on gmail point com
  */
 
+#include <array>
+#include <bit>
+#include <charconv>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <format>
+#include <limits>
+#include <numbers>
+#include <ranges>
+#include <span>
+#include <stack>
 #include <string>
 #include <string_view>
-#include <utility>
-#include <charconv>
-#include <numbers>
-#include <unordered_map>
-#include <cmath>
-#include <limits>
-#include <stack>
-#include <ranges>
-#include <array>
-#include <vector>
-#include <format>
-#include <span>
-#include <bit>
+#include <system_error>
 #include <type_traits>
-#include <chrono>
-#include <cstdlib>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
