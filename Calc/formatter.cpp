c769de37092de828755cefe02515d4d40491c9bf
HedgehogInTheCPP <a.rainman@gmail.com>
2025-09-15 22:14:10 +0000

    # TODO fix for formatter.
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@407 bea4479c-5f86-c742-a837-c53f34f90b20

--- Calc/formatter.cpp
+++ Calc/formatter.cpp
@@ -11,8 +11,36 @@
 
 [[nodiscard]] std::string Formatter::format(Value value) noexcept
 {
-	//return fmt::format(FMT_COMPILE("{:.{}g}"), value, std::numeric_limits<Value>::digits10 - 2 - 1);
-	return fmt::format(FMT_COMPILE("{}"), value);
+	//std::string s = 
+	
+	//return fmt::format(FMT_COMPILE("{:.{}g}"), value, std::numeric_limits<Value>::digits10 - 2 - 1);	// ok: 285, failed: 9
+	//return fmt::format(FMT_COMPILE("{:.{}g}"), value, std::numeric_limits<Value>::digits10);			// ok: 280, failed: 14
+	//return fmt::format(FMT_COMPILE("{:.{}g}"), value, std::numeric_limits<Value>::max_digits10);		// ok: 218, failed: 76
+	return fmt::format(FMT_COMPILE("{}"), value);														// ok: 218, failed: 76
+
+	/*
+	uint_fast16_t max_digits = std::numeric_limits<Value>::max_digits10;
+	uint_fast16_t n = 0;
+	if (s.find_first_not_of("0123456789", n) == std::string::npos)
+	{
+		s.resize(max_digits);
+		return s;
+	}
+	if (s.find_first_of('.') != std::string::npos)
+	{
+		n = s.find_first_of('.');
+		if (n < max_digits)
+		{
+			s.resize(n + 1 + max_digits - n);
+			return s;
+		}
+	}
+	else
+	{
+		s.resize(max_digits);
+		return s;
+	}
+	*/
 }
 
 #ifndef CALC_USE_ERROR_TOKEN
