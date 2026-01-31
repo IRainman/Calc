8f2e3f1ab00d36b4dbadf62a9596541dc65712c8
HedgehogInTheCPP <a.rainman@gmail.com>
2026-01-31 20:11:09 +0000

    * GUI cleanup.
    git-svn-id: https://studio2000.xyz:8443/svn/TPLTM/Lab1/Calc@508 bea4479c-5f86-c742-a837-c53f34f90b20

--- Calc/GUI.cpp
+++ Calc/GUI.cpp
@@ -29,7 +29,46 @@
 #endif
 
 #define WIN32_LEAN_AND_MEAN
-#define NOMINMAX // Ensure that we using min/max from std algorithms.
+
+#define NOGDICAPMASKS - CC_*, LC_*, PC_*, CP_*, TC_*, RC_
+#define NOVIRTUALKEYCODES - VK_ *
+//#define NOWINMESSAGES - WM_*, EM_*, LB_*, CB_*
+#define NOWINSTYLES - WS_*, CS_*, ES_*, LBS_*, SBS_*, CBS_*
+#define NOSYSMETRICS - SM_ *
+//#define NOMENUS - MF_ *
+#define NOICONS - IDI_ *
+#define NOKEYSTATES - MK_ *
+#define NOSYSCOMMANDS - SC_ *
+#define NORASTEROPS - Binary and Tertiary raster ops
+//#define NOSHOWWINDOW - SW_ *
+#define OEMRESOURCE - OEM Resource values
+#define NOATOM - Atom Manager routines
+#define NOCLIPBOARD - Clipboard routines
+#define NOCOLOR - Screen colors
+//#define NOCTLMGR - Control and Dialog routines
+#define NODRAWTEXT - DrawText() and DT_ *
+//#define NOGDI - All GDI defines and routines
+#define NOKERNEL - All KERNEL defines and routines
+//#define NOUSER - All USER defines and routines
+#define NONLS - All NLS defines and routines
+#define NOMB - MB_ * and MessageBox()
+#define NOMEMMGR - GMEM_*, LMEM_*, GHND, LHND, associated routines
+#define NOMETAFILE - typedef METAFILEPICT
+#define NOMINMAX - Macros min(a, b) and max(a, b)
+//#define NOMSG - typedef MSG and associated routines
+#define NOOPENFILE - OpenFile(), OemToAnsi, AnsiToOem, and OF_*
+#define NOSCROLL - SB_ * and scrolling routines
+#define NOSERVICE - All Service Controller routines, SERVICE_ equates, etc.
+#define NOSOUND - Sound driver routines
+#define NOTEXTMETRIC - typedef TEXTMETRIC and associated routines
+#define NOWH - SetWindowsHook and WH_ *
+//#define NOWINOFFSETS - GWL_*, GCL_*, associated routines
+#define NOCOMM - COMM driver routines
+#define NOKANJI - Kanji support stuff.
+#define NOHELP - Help engine interface.
+#define NOPROFILER - Profiler interface.
+#define NODEFERWINDOWPOS - DeferWindowPos routines
+#define NOMCX - Modem Configuration Extensions
 
 #include "targetver.h"
 
@@ -580,8 +619,7 @@ int WINAPI WinMain(const HINSTANCE hInstance, [[maybe_unused]] const HINSTANCE h
     // TODO: move this code to the core.
     /* Applications that generate floating point underflow in vector registers can benefit from setting the flush-to-zero mode rather than generating subnormal numbers in case of underflow:*/
     /* It is strongly recommended to set the flush-to-zero mode unless you have special reasons to use subnormal numbers. You may, in addition, set the denormals-are-zero mode if vector regsiters are available:*/
-    // Set flush-to-zero and denormals-are-zero mode (SSE2):
-    _mm_setcsr(_mm_getcsr() | 0x8040);
+    _mm_setcsr(_mm_getcsr() | _MM_FLUSH_ZERO_ON | _MM_DENORMALS_ZERO_ON);
 
 #ifndef CALC_USE_IME
     ImmDisableIME(0);
