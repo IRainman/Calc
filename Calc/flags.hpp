268457ddbebed4c8c5863ad38861b60f3cbb269e
HedgehogInTheCPP <a.rainman@gmail.com>
2026-09-06 04:47:16 +0300

    small improvements for future types.

--- Calc/flags.hpp
+++ Calc/flags.hpp
@@ -13,18 +13,22 @@ using EquationSize = size_t;
 
 #if _DEBUG && __STDCPP_FLOAT128_T__ == 1
 using Value = std::float128_t;
-#define CALC_USE_128_BIT_FLOAT 1
 #warning "WIP: Calc is using 64-bit double implementation in many places."
+using Integer = std::int128_t;
+using UInteger = std::uint128_t;
+
+#define CALC_USE_128_BIT_FLOAT 1
+constexpr auto huge_value_precision = 1e33;
+constexpr auto small_value_precision = 1e-33;
 #else
 #if __STDCPP_FLOAT64_T__ == 1
 using Value = std::float64_t;
-using Integer = std::int64_t;
-using UInteger = std::uint64_t;
 #else
 using Value = double;
+#endif
 using Integer = std::int64_t;
 using UInteger = std::uint64_t;
-#endif
+
 #define CALC_USE_128_BIT_FLOAT 0
 constexpr auto huge_value_precision = 1e15;
 constexpr auto small_value_precision = 1e-15;
@@ -35,9 +39,6 @@ using ParamCount = char;
 //---------------------------------------------------------------------------
 // TODO
 // #define CALC_USE_ERROR_TOKEN // WIP
-//---------------------------------------------------------------------------
-// TODO: needs to be refactored because code is very complicated:
-#define CALC_ALLOW_UNICODE_IN_GUI // allow Unicode input in GUI
 //---------------------------------------------------------------------------
 #ifdef _DEBUG
 #define CALC_TESTS_ENABLED
