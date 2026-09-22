1442b7d2d8fc32bf5e00d81f5f2078ad8c143bee
HedgehogInTheCPP <a.rainman@gmail.com>
2026-09-22 21:26:52 +0300

    Warnings additional cleanup.

--- Calc/pch.hpp
+++ Calc/pch.hpp
@@ -16,6 +16,7 @@
 //---------------------------------------------------------------------------
 // Don't needs to check _MSVC_LANG it's should be set by the compiler options
 static_assert(__cplusplus >= 202302L, "Calc is C++ latest-edge standard app");
+
 //---------------------------------------------------------------------------
 // My fast-float fork and Zmij uses those instructions:
 #if defined(_M_IX86) || defined(__i686__) || defined(__i386__) ||              \
@@ -30,6 +31,7 @@ static_assert(__cplusplus >= 202302L, "Calc is C++ latest-edge standard app");
 #define __SSE4_2__ 1 // explicitely enable SSE4.2 for 64 bit builds
 #endif
 #endif
+
 //---------------------------------------------------------------------------
 // Calc compile options:
 // clang-format off
@@ -42,24 +44,27 @@ static_assert(__cplusplus >= 202302L, "Calc is C++ latest-edge standard app");
 #pragma warning(disable : 4668) // is not defined as a preprocessor macro, replacing with '0' for 'directives'
 #pragma warning(disable : 4710) // ' ': function not inlined
 #pragma warning(disable : 4711) // function ' ' selected for automatic inline expansion
+#pragma warning(disable : 4820) // 'x': 'n' bytes padding added after data member 'y'
 #pragma warning(disable : 5026) // 'x': move constructor was implicitly defined as deleted
 #pragma warning(disable : 5027) // move assignment operator was implicitly defined as deleted
 #pragma warning(disable : 5030) // attribute ' ' is not recognized
 #pragma warning(disable : 5045) // warning for unsafe buffer usage
 #pragma warning(disable : 5222) // all unscoped attribute names are reserved for future standardization
 // clang-format on
+
 //---------------------------------------------------------------------------
 #include "flags.hpp"
 // add headers that you want to pre-compile here:
-#include <array>         // Calc and Win32 GUI
+#include <array>         // Calc and Win32 GUI (Layout)
 #include <limits>        // Calc
-#include <new>           // GUI (Normalizer), Formatter
+#include <new>           // Win32 GUI (Normalizer), Formatter
 #include <numbers>       // Calc
-#include <optional>      // Win32 GUI RegRead helper
+#include <numeric>       // Calc
+#include <optional>      // Win32 GUI (RegRead)
 #include <ranges>        // Calc
 #include <span>          // Calc
 #include <stdfloat>      // Calc
-#include <string>        // Win32 GUI
+#include <string>        // Win32 GUI (CalcApp, Normalizer)
 #include <string_view>   // Calc
 #include <unordered_map> // Calc
 #ifdef CALC_TEST_EQUATION_SOLVER
@@ -68,6 +73,7 @@ static_assert(__cplusplus >= 202302L, "Calc is C++ latest-edge standard app");
 #ifdef CALC_TESTS_ENABLED
 #include <chrono>
 #endif
+
 //---------------------------------------------------------------------------
 // Zmij compile options:
 // clang-format off
@@ -87,18 +93,17 @@ static_assert(__cplusplus >= 202302L, "Calc is C++ latest-edge standard app");
 #pragma warning(disable : 4456) // declaration of 'x' hides previous local declaration
 #pragma warning(disable : 4459) // declaration of 'x' hides global declaration
 #pragma warning(disable : 4804) // 'operator' : unsafe use of type 'type_x' in operation
-#pragma warning(disable : 4820) // 'x': 'n' bytes padding added after data member 'y'
 #pragma warning(disable : 5245) // '`anonymous-namespace'x': unreferenced function with internal linkage has been removed
 // clang-format on
 #include "../../zmij/zmij.cc"
 #pragma warning(pop)
+
 //---------------------------------------------------------------------------
 // clang-format off
 #pragma warning(push)
 #pragma warning(disable : 4061) // enumerator 'x' in switch of enum 'y' is not explicitly handled by a case label
 #pragma warning(disable : 4574) // 'x' is defined to be 'n': did you mean to use '#if x'?
 #pragma warning(disable : 4582) // constructor is not implicitly called
-#pragma warning(disable : 4820) // suppress "padding added after data member" for this struct
 // fmt compile options:
 #define FMT_HEADER_ONLY 1
 #define FMT_USE_FLOAT 0
@@ -132,10 +137,9 @@ static_assert(__cplusplus >= 202302L, "Calc is C++ latest-edge standard app");
 #include "../../fmt/include/fmt/compile.h"
 #pragma warning(pop)
 //---------------------------------------------------------------------------
+
 // fast_float compile options:
 // clang-format off
-#pragma warning(push)
-#pragma warning(disable : 4820) // suppress "padding added after data member" for this struct
 #if CALC_USE_128_BIT_FLOAT
 #warning "128-bit float type isn't supported by fast_float. The library convert any user input values to 64-bit double."
 #endif
@@ -147,7 +151,7 @@ static_assert(__cplusplus >= 202302L, "Calc is C++ latest-edge standard app");
 // Wo options Tests time is: 53278ms. Original Tests time is: 56140ms.
 // clang-format on
 #include "../../fast_float/include/fast_float/fast_float.h"
-#pragma warning(pop)
+
 //---------------------------------------------------------------------------
 /**
  * Types using for calculation.
@@ -176,15 +180,16 @@ constexpr auto huge_value_precision = 1e15;
 constexpr auto small_value_precision = 1e-15;
 #endif
 constexpr auto output_precision = std::numeric_limits<Value>::digits10;
+
 //---------------------------------------------------------------------------
 /**
  * Stack type for result formatting
  */
 using Result = std::array<char,
 #ifdef CALC_TESTS_ENABLED
-                          128 * 1024
+                          1024
 #else
-                          std::hardware_destructive_interference_size
+                          zmij::double_buffer_size
 #endif
                           >;
 //---------------------------------------------------------------------------
